51437fe701f3e35b13211438889693cb427b2bc6
allenk <dev.allenk@gmail.com>
2020-06-07 17:27:20 +0900

    feat: styling Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,15 @@
 import React from "react";
 import * as S from "./styles";
+import TextButton from "@components/Button/TextButton";
 
 export default function Header() {
-  return <S.Header>this is header</S.Header>;
+  return (
+    <S.Header>
+      <S.Logo>M</S.Logo>
+      <S.Menus>
+        <ion-icon name="search-outline"></ion-icon>
+        <TextButton>Login</TextButton>
+      </S.Menus>
+    </S.Header>
+  );
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,3 +1,29 @@
 import styled from "styled-components";
 
-export const Header = styled.header``;
+export const Header = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  padding: 0 1em;
+  width: 100vw;
+  height: 56px;
+`;
+
+export const Logo = styled.span`
+  width: 35px;
+  height: 35px;
+  margin-left: 0.5em;
+
+  font-size: 2rem;
+  line-height: 37px;
+  text-align: center;
+
+  background: #000;
+  color: #fff;
+`;
+
+export const Menus = styled.div`
+  display: flex;
+  align-items: center;
+`;

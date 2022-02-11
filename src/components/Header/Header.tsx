7b7a807293ea31aac0288010571e16069f97abbe
dev.allenk <dev.allenk@gmail.com>
2022-02-11 20:55:49 +0900

    feat: Added Home Link on Logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import * as S from './styles'
 import TextButton from '@components/Button/TextButton'
+import Link from 'next/link'
 
 export default function Header() {
   return (
     <S.Header>
-      <S.Logo>M</S.Logo>
+      <Link href={'/'}>
+        <S.Logo>M</S.Logo>
+      </Link>
       <S.Menus>
         <ion-icon name="search-outline"></ion-icon>
         <TextButton>Login</TextButton>
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
 export const Header = styled.header`
   display: flex;
@@ -8,23 +8,25 @@ export const Header = styled.header`
   padding: 0 1em;
   width: calc(100vw - 2em);
   height: 56px;
-`;
+`
 
 export const Logo = styled.span`
   width: 35px;
   height: 35px;
   margin-left: 0.5em;
 
-  font-family: "Noto Serif", serif;
+  font-family: 'Noto Serif', serif;
   font-size: 1.75rem;
   line-height: 35px;
   text-align: center;
 
   background: #000;
   color: #fff;
-`;
+
+  cursor: pointer;
+`
 
 export const Menus = styled.div`
   display: flex;
   align-items: center;
-`;
+`

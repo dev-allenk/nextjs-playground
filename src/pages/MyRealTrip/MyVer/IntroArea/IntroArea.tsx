ea5b78c5bf0d11586a65170382bf515f04eb02ec
dev.allenk <dev.allenk@gmail.com>
2022-02-17 20:30:05 +0900

    refactor: Separated directory

---
+++ src/pages/MyRealTrip/MyVer/IntroArea/IntroArea.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+import { IMAGE_BASE_PATH } from '@constants/urls'
+import useIntersectRatio, { getThresholdBy } from '@hooks/useIntersectRatio'
+import * as S from './style'
+
+const MY_REAL_TRIP_IMAGE_PATH = `${IMAGE_BASE_PATH}/myrealtrip-career`
+const VIDEO_URL = 'https://career.myrealtrip.com/release/static/media/flight.f049c58c.mp4'
+
+const threshold = getThresholdBy(2)
+
+export default function IntroArea() {
+  const { setTarget, intersectRatio } = useIntersectRatio({ threshold })
+
+  const opacity = intersectRatio
+  const transform = `scale(${11 - intersectRatio * 10})`
+
+  return (
+    <S.IntroArea>
+      <S.Container ref={setTarget}>
+        <S.Video style={{ opacity }} src={VIDEO_URL} autoPlay loop muted />
+        <S.ImageContainer style={{ opacity, transform }}>
+          <S.ImageWrapper style={{ opacity, transform }}>
+            <S.BackgroundImage
+              style={{ opacity, transform }}
+              src={`${MY_REAL_TRIP_IMAGE_PATH}/intro/bg-window.png`}
+              noLazyLoad
+            />
+          </S.ImageWrapper>
+        </S.ImageContainer>
+      </S.Container>
+    </S.IntroArea>
+  )
+}
--- src/pages/MyRealTrip/MyVer/MyRealTrip.MyVer.tsx
+++ src/pages/MyRealTrip/MyVer/MyRealTrip.MyVer.tsx
@@ -1,32 +1,10 @@
-import { IMAGE_BASE_PATH } from '@constants/urls'
-import useIntersectRatio, { getThresholdBy } from '@hooks/useIntersectRatio'
+import IntroArea from './IntroArea'
 import * as S from './style'
 
-const MY_REAL_TRIP_IMAGE_PATH = `${IMAGE_BASE_PATH}/myrealtrip-career`
-const VIDEO_URL = 'https://career.myrealtrip.com/release/static/media/flight.f049c58c.mp4'
-
-const threshold = getThresholdBy(2)
-
 export default function MyRealTripMyVer() {
-  const { setTarget, intersectRatio } = useIntersectRatio({ threshold })
-
-  const opacity = intersectRatio
-  const transform = `scale(${11 - intersectRatio * 10})`
-
   return (
     <S.MyRealTrip>
-      <S.Container ref={setTarget}>
-        <S.Video style={{ opacity }} src={VIDEO_URL} autoPlay loop muted />
-        <S.ImageContainer style={{ opacity, transform }}>
-          <S.ImageWrapper style={{ opacity, transform }}>
-            <S.BackgroundImage
-              style={{ opacity, transform }}
-              src={`${MY_REAL_TRIP_IMAGE_PATH}/intro/bg-window.png`}
-              noLazyLoad
-            />
-          </S.ImageWrapper>
-        </S.ImageContainer>
-      </S.Container>
+      <IntroArea />
       <div style={{ height: '100vh' }}></div>
     </S.MyRealTrip>
   )

dd82904d86fca0710106e7a9c9bd5c2c894459b6
allenk <dev.allenk@gmail.com>
2023-06-18 17:45:13 +0900

    [nextjs-app] yarn berry to pnpm

--- services/nextjs-app/pages/_app.tsx
+++ services/nextjs-app/pages/_app.tsx
@@ -1,6 +1,5 @@
 import { AppProps } from 'next/app'
 import GlobalStyle from 'src/styles/GlobalStyle'
-import Header from '../src/components/Header'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
--- services/nextjs-app/pages/playground.ts
+++ services/nextjs-app/pages/playground.ts
@@ -1,1 +1,1 @@
-export { default } from '../src/pages/playground'
+export { default } from 'src/pages/playground'
